7ba34ff955cfded3451913fa0760cb6a6870dd9e
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-08-12 19:02:47 +0530

    Today Learnt to Print a Tree

---
+++ Tree - Generic Trees/Print a Tree.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+root = TreeNode(1)
+
+child1 = TreeNode(2)
+child2 = TreeNode(3)
+child3 = TreeNode(4)
+
+root.children.append(child1)
+root.children.append(child2)
+root.children.append(child3)
+
+def print_tree(root):
+    print(root.data)
+    for eachChild in root.children:
+        print_tree(eachChild)
+
+print_tree(root)
